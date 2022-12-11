67fafb5d5f6d0916bcdfe0365777d1a72ff6c077
CodingByGopal <gopalji028@gmail.com>
2022-12-11 16:53:38 +0530

    property binding

--- src/app/Mycomponents/mw-media-item/mw-media-item.component.ts
+++ src/app/Mycomponents/mw-media-item/mw-media-item.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mw-media-item',
   templateUrl: './mw-media-item.component.html',
-  styleUrls: ['./mw-media-item.component.css']
+  styleUrls: ['./mw-media-item.component.css'],
 })
 export class MwMediaItemComponent {
-
+  name = 'Media Item 1';
+  tag =
+    'This line is generated from mw-media-item.components.ts file by using <----- [textContent]="tag" -----> (its called property binding)';
+  wasWatched() {
+    return true;
+  }
 }
