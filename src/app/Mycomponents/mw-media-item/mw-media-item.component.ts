2675c68bbf54e1febdb6a03e2cbd8de09effe941
CodingByGopal <gopalji028@gmail.com>
2022-12-11 17:42:35 +0530

    Data passed into component

--- src/app/Mycomponents/mw-media-item/mw-media-item.component.ts
+++ src/app/Mycomponents/mw-media-item/mw-media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mw-media-item',
@@ -13,7 +13,11 @@ export class MwMediaItemComponent {
     return true;
   }
 
+  @Input() mediaItem: any;
+  @Output() delete = new EventEmitter();
+
   onDelete() {
     console.log('deleted');
+    this.delete.emit(this.mediaItem);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Friction',
+    year: 2010,
+    watchedOn: 123727771,
+    isFavorite: false,
+  };
+
+  onMediaItemDelete(mediaItem: any) {}
 }
